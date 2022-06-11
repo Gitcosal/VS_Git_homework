36ed44df48034f1fc1910ab72794d5bda3ff17d8
cosal <twinwood2011@naver.com>
2022-06-12 01:51:06 +0900

    good

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import random
-r = random.randrange(17)
+import time
 l = ["Can you cook pasta?", "Can she play guitar?", "Can he speak Chinese?", 
 "Can they swim?", "Can you drive a car?", "I have a cold, what should I do?", 
 "He is always late, what should he do?", "You failed the test, what should you do?",
@@ -8,7 +8,11 @@ l = ["Can you cook pasta?", "Can she play guitar?", "Can he speak Chinese?",
  "Who is taller, your mom or your dad?", "Which is more delicious, cola, or coffee?", 
  "Who is funnier, you or your friend.", "Which is more difficult, English or Chinese?", 
  "Which is more enjoyable, watching YouTube or watching Netflix?"]
-print(l[r])
+ 
+for i in range(10):
+    r = random.randrange(17)
+    print(l[r])
+    time.sleep(10)
 
 
 #자연스럽게
